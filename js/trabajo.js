f1969ea531cb98e79070b248761a77035b226bec
gabilan129 <gabrielraul16@gmail.com>
2022-08-10 20:46:31 -0300

    trabajo 1.06

--- js/trabajo.js
+++ js/trabajo.js
@@ -1,3 +1,11 @@
+function saludar() {
+    alert("bienvenido a intervalo gym")
+}
+
+saludar()
+
+
+
 const carrito = [];
 
 
@@ -107,96 +115,5 @@ botonFinDeCompra.addEventListener ("click", (e) => {
 
 
 
-function saludar() {
-    alert("bienvenido a intervalo gym")
-}
-
-saludar()
-
-class Suplementos {
-    constructor(tipo, marca, precio, Unidades) {
-
-        this.tipo = tipo,
-            this.marca = marca,
-            this.precio = parseFloat(precio),
-            this.Unidades = Unidades
-    }
-};
-
-const productos = [];
-
-        
-let precio = 0;
 
 
-let producto = prompt("ingrese el producto que desea \nproteina: star,hard o animal \ncreatina star,hard o animal\nquema grasa star,hard o animal ");
-
-function compra() {
-
-
-    while (producto != "fin") {
-        switch (producto) {
-            case "proteina star":
-                productos.push(new Suplementos("proteina", "Star Nutrition", 5000, 30));
-                console.log("el precio de la proteina Star Nutrition es de $5000");
-                precio = precio + 5000;
-                break;
-            case "proteina animal":
-                productos.push(new Suplementos("proteina", "animal", 6000, 30));
-                console.log("el precio de la proteina animal es de $6000");
-                precio = precio + 6000;
-                break;
-            case "proteina hard":
-                productos.push(new Suplementos("proteina", "hard", 1500, 30));
-                console.log("el precio de la proteina hard es de $1500");
-                precio = precio + 1500;
-                break;
-            case "creatina star":
-                productos.push(new Suplementos("creatina", "Star Nutrition", 720, 150));
-                console.log("el precio de la creatina Star Nutrition es de $720");
-                precio = precio + 720;
-                break;
-            case "creatina animal":
-                productos.push(new Suplementos("creatina", "animal", 800, 120));
-                console.log("el precio de la creatina animal es de $800");
-                precio = precio + 800;
-                break;
-            case "creatina hard":
-                productos.push(new Suplementos("creatina", "hard", 500, 150));
-                console.log("el precio de la creatina hard es de $500");
-                precio = precio + 500;
-                break;
-            case "quema grasa star":
-                productos.push(new Suplementos("quema grasa", "Star Nutrition", 1720, 60));
-                console.log("el precio del quema grasa Star Nutrition es de $1720");
-                precio = precio + 1720;
-                break;
-            case "quema grasa animal":
-                productos.push(new Suplementos("quema grasa", "animal", 1800, 60));
-                console.log("el precio del quema grasa es de $1800");
-                precio = precio + 1800;
-                break;
-            case "quema grasa hard":
-                productos.push(new Suplementos("quema grasa", "hard", 3500, 60));
-                console.log("el precio del quema grasa hard es de $3500");
-                precio = precio + 3500;
-                break;
-            default:
-                console.log("no hay producto en stock")
-                break;
-        }
-        producto = prompt("ingrese el nuevo producto o fin para la suma total")
-    }
-
-    const productosUnidades = productos.filter((marc) => marc.Unidades >= 60)
-    console.log("los siguientes productos rinden mas de 2 meses de uso")
-    console.log(productosUnidades)
-
-    let precioTotal = precio * 1.21;
-
-    console.log("El totales de $" + precioTotal);
-
-
-}
-
-compra();
